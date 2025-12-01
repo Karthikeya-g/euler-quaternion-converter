b5888e5afac8a7d6257d3b55fd76f26162c755e8
Karthikeya-g <na21b026@smail.iitm.ac.in>
2025-12-01 15:57:19 +0530

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .rotation_converter import RotationConverter, EulerAngles
+
+# This defines what gets imported when someone runs: "from src import *"
+__all__ = ["RotationConverter", "EulerAngles"]
